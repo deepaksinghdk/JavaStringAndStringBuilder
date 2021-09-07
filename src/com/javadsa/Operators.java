4784b5f90e3746930ef173035da4b7ce25cc7c03
deepak <iamnew2014@gmail.com>
2021-09-07 23:41:03 +0530

    first commit

---
+++ src/com/javadsa/Operators.java
@@ -0,0 +1,20 @@
+package com.javadsa;
+
+public class Operators {
+    public static void main(String[] args) {
+        //if we put the char and add with the + operator then it will add the assaai value of that character
+        System.out.println('c'+'d');
+        System.out.println();
+        //if we add two Sytring then it will simply concatinate the values
+        System.out.println("a" + "b");
+        System.out.println();
+        //if we add one string and one one integer trhen it will .also concatinate the values
+        System.out.println("a" + 1);
+        System.out.println();
+        //if we add one char and one int then then it will add that int value to the ascaii value of char
+        System.out.println('a' + 3);
+        System.out.println();
+        //converting it into char value
+        System.out.println((char)('a' + 3));
+    }
+}
