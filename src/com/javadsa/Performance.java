e364fefef5ed93924377116a03d34b7c9994476b
deepak <iamnew2014@gmail.com>
2021-09-08 00:11:46 +0530

    first commit

---
+++ src/com/javadsa/Performance.java
@@ -0,0 +1,11 @@
+package com.javadsa;
+
+public class Performance {
+    public static void main(String[] args) {
+        String str = "";
+        for (int i = 0; i < 26; i++) {
+            char ch = (char)('a' + i);
+            System.out.println(ch);
+        }
+    }
+}
